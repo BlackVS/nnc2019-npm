4d5c856bf1264cc48401e972ccc94a1844e7a701
BlackVS <vvs@coders.in.ua>
2020-01-11 06:40:39 +0200

    Windows bat file added

---
+++ packages/pmon1/bin/flash_win.bat
@@ -0,0 +1,12 @@
+@echo Badge flasher
+@echo You need:
+@echo  * python3 installed
+@echo  * esptool package installed
+@echo Install esptool:
+pip3 install esptool
+@echo Check com ports:
+mode
+@SET port=COM4
+
+esptool.py --chip esp32 --port %port% --baud 2000000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 partitions.bin
+
