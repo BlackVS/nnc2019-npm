81b33b7840824db1984d1bf36da4b8c89bbc8673
BlackVS <vvs@coders.in.ua>
2020-01-11 15:43:13 +0200

    qio->dio + BadgeOS

--- packages/example2/bin/flash_win.bat
+++ packages/example2/bin/flash_win.bat
@@ -8,5 +8,5 @@ pip3 install esptool
 mode
 @SET port=COM4
 
-esptool.py --chip esp32 --port %port% --baud 2000000 --before default_reset --after hard_reset write_flash -z --flash_mode qio --flash_freq 40m --flash_size detect 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 partitions.bin
+esptool.py --chip esp32 --port %port% --baud 2000000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 partitions.bin
 
--- packages/mega/bin/flash_linux.sh
+++ packages/mega/bin/flash_linux.sh
@@ -11,5 +11,5 @@
 # 0x10000 /data/nnc/badge-os-cpp/build/badge-00.bin 0x8000 
 # /data/nnc/badge-os-cpp/build/partitions.bin
 
-esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 2000000 --before default_reset --after hard_reset write_flash -z --flash_mode qio --flash_freq 40m --flash_size detect 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 partitions.bin
+esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 2000000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 partitions.bin
 
